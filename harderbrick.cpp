3b9f29803e9e7d911440cf6e2430f42b355e4470
HyperMn <hypermn@163.com>
2018-06-18 16:24:30 +0800

    Initial commit

---
+++ harderbrick.cpp
@@ -0,0 +1,7 @@
+#include "harderbrick.h"
+
+HarderBrick::HarderBrick(QGraphicsItem* parent):
+  AbstractBrick(5,QColor(50,50,50,100),parent)
+{
+
+}
---
+++ hardestbrick.cpp
@@ -0,0 +1,7 @@
+#include "hardestbrick.h"
+
+HardestBrick::HardestBrick(QGraphicsItem* parent):
+  AbstractBrick(7,QColor(20,20,20,100),parent)
+{
+
+}
